fa55439b81b3e1fbe6c4713a53adb238de3ed8fd
unknown <alamin.cse.ru2018@gmail.com>
2022-08-12 11:58:50 +0600

    Crud app added

---
+++ app/Http/Controllers/IngredientsController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ingradient;
+use Illuminate\Http\Request;
+
+class IngredientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ingredients = Ingradient::with('recipes')->get();
+
+        return view('Ingredients.index', ['ingredients' => $ingredients]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ingredient  $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ingredient  $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ingredient  $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ingredient  $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ingredient $ingredient)
+    {
+        //
+    }
+}
